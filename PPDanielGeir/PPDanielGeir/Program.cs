827d3aaad321b102605b4e5e72233018f77553ee
Geir Okkenhaug Jerstad <geokkjer@gmail.com>
2025-01-09 11:35:33 +0100

    Started

--- PPDanielGeir/PPDanielGeir/Program.cs
+++ PPDanielGeir/PPDanielGeir/Program.cs
@@ -1,3 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+﻿/*2) Butikkoversikt
+    Denne appen skal gi en oversikt over hvilke butikker som finnes, hva som er spesialiteten deres (klær, matvarer, leker etc) 
+    og prisnivået varene ligger på (1= billig 2=midt på treet 3=sinnsykt dyrt)
+    Man skal også kunne legge til nye butikker, og få mulighet til å velge å se en 
+    liste over alle butikker, velge å se kun de billigste, velge å se kun de som 
+    er midt på treet prismessig eller velge å se bare de butikkene som er rangert 
+    som sinnsykt dyre.
+    BONUS
+    Gjør så man også kan sortere/velge å se butikker som har en viss spesialitet f.eks klær i tillegg til å kunne sortere på pris. 
+    Kjekt å se hvor man kan kjøpe billig mat og dyre designer klær, hehe ;)*/
+    using PPDanielGeir;
+    var shops =  new List<Shop>();
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Welcome to Daniel & Geir's list of shops !");
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("1.) Vis alle butikkene");
+        Console.WriteLine("2.) Legg til ny butikk");
+        Console.WriteLine("X.) Avslutt");
+        var input  = Console.ReadLine();
+        switch (input)
+        {
+            case "1":
+                break;
+            case "2":
+                break;
+            case "X":
+                System.Environment.Exit(0);
+                break;
+        }
+    }
---
+++ PPDanielGeir/PPDanielGeir/Shop.cs
@@ -0,0 +1,15 @@
+namespace PPDanielGeir;
+
+public class Shop 
+{
+    public string ShopName { get; set; }
+    public string Speciality { get; set; }
+    public int PriceLevel { get; set; }
+
+    public Shop(string shopName, string speciality, int priceLevel)
+    {
+        ShopName = shopName;
+        Speciality = speciality;
+        PriceLevel = priceLevel;
+    }
+}
